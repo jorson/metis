98d2f4a906a01614a3d29018281dc30c4c814f88
wangcx <wangcx@101.com>
2014-09-05 10:41:07 +0800

    添加测试程序

--- ServerComponent/SysLogMonitor/src/test/java/com/metis/monitor/syslog/kafka/KafkaWriteTest.java
+++ ServerComponent/SysLogMonitor/src/test/java/com/metis/monitor/syslog/kafka/KafkaWriteTest.java
@@ -15,6 +15,7 @@ import java.util.*;
  */
 public class KafkaWriteTest {
 
+    private static final int[] userId = new int[] {1000, 2000, 3000, 4000, 5000};
     private static final int[] appId = new int[] {7, 19, 20, 35, 17};
     private static final int[] logLevel = new int[] {1, 2, 3, 4, 5};
     private static final String[] logMessage = new String[]
@@ -37,8 +38,76 @@ public class KafkaWriteTest {
     private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
     @Test
+    public void writeVisitLogToKafka()  throws InterruptedException, UnsupportedEncodingException {
+        long events = 3000;
+        Properties props = new Properties();
+        props.put("metadata.broker.list", "192168-205213:9091,192168-205213:9092,192168-205213:9093");
+        props.put("serializer.class", "kafka.serializer.StringEncoder");
+        props.put("partitioner.class", "com.metis.monitor.syslog.kafka.SimplePartitioner");
+        props.put("request.required.acks", "1");
+
+        ProducerConfig config = new ProducerConfig(props);
+        Producer<String, String> producer = new Producer<String, String>(config);
+        List<KeyedMessage<String, String>> dataList = new ArrayList<KeyedMessage<String, String>>();
+
+        for(long event = 0; event < events; event++){
+            String message = buildPageVisit();
+            KeyedMessage<String, String> data = new KeyedMessage<String, String>("sys_log", message);
+            producer.send(data);
+            //dataList.add(data);
+            //每发送一笔记录停止一下
+            Thread.sleep(500);
+            System.out.println("Write Event:" + event);
+        }
+        //producer.send(dataList);
+        System.out.println("Write Over!");
+        producer.close();
+    }
+
+    @Test
+    public void writeToKafkaThread() throws InterruptedException, UnsupportedEncodingException {
+        for(int i=0; i<2; i++) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        long events = 1;
+                        Properties props = new Properties();
+                        props.put("metadata.broker.list", "192168-205213:9091,192168-205213:9092,192168-205213:9093");
+                        props.put("serializer.class", "kafka.serializer.StringEncoder");
+                        props.put("partitioner.class", "com.metis.monitor.syslog.kafka.SimplePartitioner");
+                        props.put("request.required.acks", "1");
+
+                        ProducerConfig config = new ProducerConfig(props);
+                        Producer<String, String> producer = new Producer<String, String>(config);
+                        List<KeyedMessage<String, String>> dataList = new ArrayList<KeyedMessage<String, String>>();
+
+                        for(long event = 0; event < events; event++){
+                            String message = buildOriginalSysLog();
+                            KeyedMessage<String, String> data = new KeyedMessage<String, String>("sys_log", message);
+                            producer.send(data);
+                            //dataList.add(data);
+                            //每发送一笔记录停止一下
+                            Thread.sleep(500);
+                            System.out.println("Write Event:" + event);
+                        }
+                        //producer.send(dataList);
+                        System.out.println("Write Over!");
+                        producer.close();
+                    } catch(Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            });
+            thread.start();
+        }
+        Thread.sleep(20000000);
+    }
+
+
+    @Test
     public void writeToKafka() throws InterruptedException, UnsupportedEncodingException {
-        long events = 1;
+        long events = 300;
         Properties props = new Properties();
         props.put("metadata.broker.list", "192168-205213:9091,192168-205213:9092,192168-205213:9093");
         props.put("serializer.class", "kafka.serializer.StringEncoder");
@@ -61,32 +130,6 @@ public class KafkaWriteTest {
         //producer.send(dataList);
         System.out.println("Write Over!");
         producer.close();
-
-/*        long events = 10;
-        Random rnd = new Random();
-
-        Properties props = new Properties();
-        props.put("metadata.broker.list", "192168-072166:9091,192168-072166:9092,192168-072166:9093");
-        props.put("serializer.class", "kafka.serializer.StringEncoder");
-        //props.put("partitioner.class", "com.metis.monitor.syslog.kafka.SimplePartitioner");
-        props.put("request.required.acks", "1");
-
-        ProducerConfig config = new ProducerConfig(props);
-
-        Producer<String, String> producer = new Producer<String, String>(config);
-        List<KeyedMessage<String, String>> dataList = new ArrayList<KeyedMessage<String, String>>();
-
-        for(long event = 0; event < events; event++){
-            long runtime = new Date().getTime();
-            String ip = "192.168.206." + rnd.nextInt(255);
-            String msg = runtime + ", this is demo #" + ip;
-            KeyedMessage<String, String> data = new KeyedMessage<String, String>("tp_test", ip, msg);
-            dataList.add(data);
-        }
-        producer.send(dataList);
-
-        System.out.println("Write Over!" + Thread.currentThread().getId());
-        producer.close();*/
     }
 
     private String buildOriginalSysLog() throws UnsupportedEncodingException {
@@ -98,5 +141,23 @@ public class KafkaWriteTest {
                 logCallStack[rndNum],
                 dateFormat.format(new Date()));
         return URLEncoder.encode(result, "UTF-8");
+    }
+
+    /**
+     * 将原始输入字符串分解,并转换为实体
+     * 原始字符串格式: [UcCode]\t[UserId]\t[AppId]\t[TerminalCode]\t[IpAddress]\t[ReferPage]\t[visitPage]\t[visitPageParam]\t[VisitTime]
+     */
+
+    private String buildPageVisit() throws UnsupportedEncodingException {
+        int rndNum = random.nextInt(5);
+        String result = String.format("auc\t%d\t%d\t%d\t35124567\t%s\t%s\t%s\t%s",
+                userId[rndNum],
+                appId[rndNum],
+                logLevel[rndNum],
+                logMessage[rndNum],
+                logCallStack[rndNum],
+                logMessage[rndNum],
+                dateFormat.format(new Date()));
+        return result;
     }
 }
